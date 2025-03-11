e6384c1ab2cbd88b35bb9f15448c00b9dc8b0fa4
Olesia Dacenko <o.khlivnuk@gmail.com>
2025-03-11 16:03:23 +0200

    333

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
+console.log(form);
 
+form.addEventListener("submit", foo);
+
+function foo(event) {
+  event.preventDefault();
+  const email = event.target.elements.email.value;
+  const password = event.target.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+  const res = {
+    email: email.trim(),
+    password: password.trim(),
+  };
+  console.log(res);
+  form.reset();
+}
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const fonColor = document.querySelector(".color");
+const body = document.body;
+
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  // fonColor.textContent = randomColor;
+});
